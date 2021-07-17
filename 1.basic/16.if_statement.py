160083827b891e5304560aea7916f24f524f641d
01one <84787013+01one@users.noreply.github.com>
2021-07-17 11:12:37 +0600

    Create 16.if_statement.py

---
+++ 1.basic/16.if_statement.py
@@ -0,0 +1,8 @@
+x=eval(input("Enter a value of 'x':"))
+y=eval(input("Enter another value of 'y':"))
+if x>y:
+    print("x is bigger than y")
+if x<y:
+    print("x is smaller than y")
+if x==y:
+    print("The value of x and y is equal")
