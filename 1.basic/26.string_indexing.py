1ed194487401f0223ab34ec78a31ad27eb685fc2
01one <84787013+01one@users.noreply.github.com>
2021-07-18 18:57:18 +0600

    Create 26.string_indexing.py

---
+++ 1.basic/26.string_indexing.py
@@ -0,0 +1,14 @@
+x="Hello"
+print(x[0]) #print H
+
+print(x[-1])#print o
+
+print(x[:]) #print Hello
+
+print(x[:2])#print He
+
+print(x[2:])#print llo
+
+print(x[0:4:2])#print Hl #from 0 to 4 difference 2
+
+print(x[::-1])#print olleH
