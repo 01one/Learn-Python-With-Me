4e0b7cfae7072e387448bd3f0e99598fc0445574
01one <84787013+01one@users.noreply.github.com>
2021-07-18 19:25:07 +0600

    Create 28.string_methods.py

---
+++ 1.basic/28.string_methods.py
@@ -0,0 +1,20 @@
+x="Hello"
+
+x=x.lower()
+print(x)    #hello
+
+x=x.upper()
+print(x)    #HELLO
+
+a = x.count('L')
+print(a)    #2
+
+b=x.index('O')
+print(b)    #4
+
+x=x.replace('L','l')
+print(x)    #HEllO
+
+#string is immutable.
+x=x[:]+" world"
+print(x)    #HEllO world
