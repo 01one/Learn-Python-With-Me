ad7a82dcc01ab3400231a37ff91325e281551557
01one <84787013+01one@users.noreply.github.com>
2021-07-22 21:19:21 +0600

    Create 31.dictionary.py

---
+++ 1.basic/31.dictionary.py
@@ -0,0 +1,6 @@
+word={"Aurora":"A natural electrical phenomenon characterized by the appearance of streamers of reddish or greenish light in the sky,\nusually near the northern or southern magnetic pole.",
+"Bubbly":"(especially of a woman or girl) Attractively full of energy and enthusiasm",
+"Cascade":"A small waterfall",
+"Devotion":"Loyalty and love or care for someone or something"}
+
+print(word['Aurora'])
